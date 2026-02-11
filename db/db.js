d1030d66ca84110f935c90e0bbfe8b83f7a87e80
Vaishali <bvaishali382@gmail.com>
2026-02-12 05:12:49 +0530

    mongo error resolving

--- db/db.js
+++ db/db.js
@@ -1,12 +1,16 @@
+
 import mongoose from 'mongoose';
 
 const db = async () => {
-    await mongoose.connect(`mongodb+srv://bvaishali382_db_user:7XbF0uac84ASCK5t@cluster0.iif7bjq.mongodb.net/?appName=Cluster0`).then( () => {
-        console.log(`Server is connected to the Database`);
-    }
-    ).catch((err) => {
-        console.log(err);
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
+    console.log("Server is connected to the Database");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
 };
 
 export default db;
